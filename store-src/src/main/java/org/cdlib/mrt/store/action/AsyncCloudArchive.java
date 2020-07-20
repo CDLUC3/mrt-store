7673b93c56e6f9f96b14a968e2a67871d74e58d5
replic <replic@uc3-mrtsandbox2-stg.cdlib.org>
2020-07-20 15:45:39 -0700

    Delete temp directory with created zip

--- store-src/src/main/java/org/cdlib/mrt/store/action/AsyncCloudArchive.java
+++ store-src/src/main/java/org/cdlib/mrt/store/action/AsyncCloudArchive.java
@@ -305,6 +305,10 @@ public class AsyncCloudArchive
             logger.logError(msgx, 2);
 
         } finally {
+            // delete zip and base
+            try {
+                FileUtil.deleteDir(workBase);
+            } catch (Exception ex) { }
         }
 
     }
