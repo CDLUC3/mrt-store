b58a9445041cd0dbaec554fa80fac59e3819c38a
Terry Brady <terrence.brady@ucop.edu>
2022-06-24 14:12:48 -0700

    refine test init

--- store-it/src/test/java/org/cdlib/mrt/store/ServiceDriverIT.java
+++ store-it/src/test/java/org/cdlib/mrt/store/ServiceDriverIT.java
@@ -3,10 +3,13 @@ package org.cdlib.mrt.store;
 import org.junit.Test;
 
 import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpResponseException;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.BasicResponseHandler;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.File;
 import java.io.Writer;
@@ -15,7 +18,7 @@ import static org.junit.Assert.*;
 
 public class ServiceDriverIT {
         private int port = 8080;
-        private String cp = "/store";
+        private String cp = "store";
 
         public ServiceDriverIT() {
                 try {
@@ -25,46 +28,37 @@ public class ServiceDriverIT {
                 }
         }
 
-        public String getContent(String url) {
+        public String getContent(String url) throws HttpResponseException, IOException {
                 try (CloseableHttpClient client = HttpClients.createDefault()) {
                     HttpGet request = new HttpGet(url);
                     HttpResponse response = client.execute(request);
+                    assertEquals(200, response.getStatusLine().getStatusCode());
 
-                    return new BasicResponseHandler().handleResponse(response).trim();
-                } catch(Exception e) {
-                    System.err.println(e.getMessage());
+                    String s = new BasicResponseHandler().handleResponse(response).trim();
+                    assertFalse(s.isEmpty());
+                    return s;
                 }
-                return "";
+        }
+
+        public JSONObject getJsonContent(String url) throws HttpResponseException, IOException, JSONException {
+                String s = getContent(url);
+                JSONObject json =  new JSONObject(s);
+                assertNotNull(json);
+                return json;
         }
 
         @Test
-        public void SimpleTest() throws IOException {
+        public void SimpleTest() throws IOException, JSONException {
                 String url = String.format("http://localhost:%d/%s/state?t=json", port, cp);
-                String resp = "";
-                try (CloseableHttpClient client = HttpClients.createDefault()) {
-                        HttpGet request = new HttpGet(url);
-                        HttpResponse response = client.execute(request);
-                        assertEquals(200, response.getStatusLine().getStatusCode());
-                        resp = new BasicResponseHandler().handleResponse(response).trim();
-                }
-                
-                System.out.println(resp);
-                assertFalse(resp.isEmpty());
+                JSONObject json = getJsonContent(url);
+                assertTrue(json.has("sto:storageServiceState"));
         }
 
         @Test
-        public void SimpleTest7777() throws IOException {
+        public void SimpleTest7777() throws IOException, JSONException {
                 String url = String.format("http://localhost:%d/%s/state/7777?t=json", port, cp);
-                String resp = "";
-                try (CloseableHttpClient client = HttpClients.createDefault()) {
-                        HttpGet request = new HttpGet(url);
-                        HttpResponse response = client.execute(request);
-                        assertEquals(200, response.getStatusLine().getStatusCode());
-                        resp = new BasicResponseHandler().handleResponse(response).trim();
-                }
-                
-                System.out.println(resp);
-                assertFalse(resp.isEmpty());
+                JSONObject json = getJsonContent(url);
+                assertTrue(json.has("nod:nodeState"));
         }
 
 }
