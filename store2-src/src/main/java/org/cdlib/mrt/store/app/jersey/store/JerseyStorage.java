cef8e0ce36461a12930579aaaf61d163c768e9bf
dloy <dloy@ucop.edu>
2026-04-14 10:52:39 -0700

    sdsc hange 3.0.2-fss3b

--- store2-src/src/main/java/org/cdlib/mrt/store/app/jersey/store/JerseyStorage.java
+++ store2-src/src/main/java/org/cdlib/mrt/store/app/jersey/store/JerseyStorage.java
@@ -26,6 +26,8 @@ INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
 CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
 ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
 OF THE POSSIBILITY OF SUCH DAMAGE.
+---------------
+Force rebuild: 260414-3.0.2-fss3b
 *********************************************************************/
 package org.cdlib.mrt.store.app.jersey.store;
 
