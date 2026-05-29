5b91d88c034b1744b9da6e25496b55460c71f007
mreyescdl <skidly_2@yahoo.com>
2026-05-29 11:37:55 -0700

    debug

--- store2-src/src/main/java/org/cdlib/mrt/store/action/CloudArchive.java
+++ store2-src/src/main/java/org/cdlib/mrt/store/action/CloudArchive.java
@@ -82,7 +82,8 @@ public class CloudArchive
     private static final String MESSAGE = NAME + ": ";
 
     private static final String NL = System.getProperty("line.separator");
-    private static final boolean DEBUG = false;
+    // private static final boolean DEBUG = false;
+    private static final boolean DEBUG = true;
     
     protected File workBase = null;
     protected File copyBase = null;
